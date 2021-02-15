40fc5e99e7773e35ec1c45e750353910ce78b7ed
Michalis Koutridis <michalis.koutridis@orfium.com>
2021-02-15 11:19:38 +0200

    feat(pages): composes home page and adds new pages placeholders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
+import User from "../components/User/User";
+import Footer from "../components/Footer/Footer";
+import InfoSection from "../components/InfoSection/InfoSection";
 
 export default function Home() {
-  return <main>
-    Portfolio App
-  </main>
+  return (
+    <main>
+      <User />
+      <InfoSection />
+      <Footer />
+    </main>
+  );
 }
