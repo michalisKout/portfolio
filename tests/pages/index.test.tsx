ee4f4a01652a9a270c02f5d9e6179d4b1ab6a7ea
Michalis Koutridis <michalis.koutridis@orfium.com>
2021-02-21 19:25:25 +0200

    fix(tests):  fixes tests

--- tests/pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "../../pages";
-import userMock from "__mocks__/userMock";
-describe("Home page", () => {
-  it("renders Home page", () => {
-    const { asFragment } = render(<Home userDetails={userMock} />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
