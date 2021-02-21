d354c857bf240f95335ce5c209b0ff78c1e326e9
Michalis Koutridis <michalis.koutridis@orfium.com>
2021-02-21 21:35:58 +0200

    ci(next): configs next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+  basePath: '/',
+  assetPrefix: '/',
   async redirects() {
     return [
       {
