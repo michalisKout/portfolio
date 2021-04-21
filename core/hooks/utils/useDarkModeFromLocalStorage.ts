7ac141f562e5a68c3bf2723a3a640b379fca8004
michalis <michalis.koutridis@gmail.com>
2021-04-22 02:01:32 +0300

    build(color-mode): fixes build issue in ts

--- core/hooks/utils/useDarkModeFromLocalStorage.ts
+++ core/hooks/utils/useDarkModeFromLocalStorage.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import localStorageUtils from '../../utils/localStorage';
 
-const DARK_MODE_CONFIG = {
+const DARK_MODE_CONFIG: Record<string, boolean> = {
   dark: true,
   normal: false,
 };
-
+const DEFAULT_DARK_MODE = false;
 export const useDarkModeFromLocalStorage = () => {
-  const [isDarkMode, setDarkMode] = useState(false);
+  const [isDarkMode, setDarkMode] = useState(DEFAULT_DARK_MODE);
 
   useEffect(() => {
     const colorMode = localStorageUtils.getLocalStorageValue('colorMode');
-    const isDarkModeStored = DARK_MODE_CONFIG[colorMode];
+    const isDarkModeStored = colorMode ? DARK_MODE_CONFIG[colorMode] : DEFAULT_DARK_MODE;
 
     if (isDarkModeStored !== isDarkMode) {
       setDarkMode(isDarkModeStored);
