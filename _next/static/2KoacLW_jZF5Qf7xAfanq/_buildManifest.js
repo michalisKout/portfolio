d84ad2d38e7f93d3c7f40c39d55f40d30e880c6e
michalisKout <50381321+michalisKout@users.noreply.github.com>
2021-04-21 23:42:15 +0000

    Deploying to gh-pages from @ michalisKout/michalisKout.github.io@5f1a52930a60ee10419fcd3ccd181d06eff94dd3 🚀

---
+++ _next/static/2KoacLW_jZF5Qf7xAfanq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d6bc336cad17f2c76d58.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-83b152d1d35edd537569.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/kvb_PD7v_h-_ZKSkr3TSl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-aa4d179d1ae8981bb6ec.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-83b152d1d35edd537569.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
